0f8c42381e05a39d862b2fc0aeb042211d63479c
bbangjun <lucid9dawn@gmail.com>
2023-07-17 22:33:15 +0900

    feat: setup

---
+++ src/main/java/com/myadd/myadd/MyaddApplication.java
@@ -0,0 +1,13 @@
+package com.myadd.myadd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyaddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyaddApplication.class, args);
+	}
+
+}
