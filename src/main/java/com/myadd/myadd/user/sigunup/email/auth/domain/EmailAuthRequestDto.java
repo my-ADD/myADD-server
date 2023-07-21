9ab665fe44a68309f4d031382ce2dc5312c37840
bbangjun <lucid9dawn@gmail.com>
2023-07-21 23:35:59 +0900

    feat(EmailAuthRequestDto): 이메일 인증 요청을 위한 dto 구현

--- src/main/java/com/myadd/myadd/user/sigunup/email/auth/domain/EmailAuthRequestDto.java
+++ src/main/java/com/myadd/myadd/user/sigunup/email/auth/domain/EmailAuthRequestDto.java
@@ -2,8 +2,11 @@ package com.myadd.myadd.user.sigunup.email.auth.domain;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class EmailAuthRequestDto {
 
-
+    @NotEmpty(message = "이메일을 입력해주세요")
+    public String email;
 }
