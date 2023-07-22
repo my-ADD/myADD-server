27a7db9a00d60bb2d48e92caa24b52da704c928f
HideOnCodec <tlsdmsgp33@naver.com>
2023-07-22 17:27:59 +0900

    feat(PostEntity) : 포토카드 엔테티 어노테이션 수정 및 dto 함수 구현

--- src/main/java/com/myadd/myadd/post/domain/PostEntity.java
+++ src/main/java/com/myadd/myadd/post/domain/PostEntity.java
@@ -1,9 +1,9 @@
 package com.myadd.myadd.post.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.myadd.myadd.post.search.dto.PostSearchDto;
 import com.myadd.myadd.user.domain.UserEntity;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -14,36 +14,54 @@ import java.time.LocalDateTime;
 @Table(name="Post")
 public class PostEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long post_id;
 
-    @ManyToOne
-    @JoinColumn(name="user_id")
+    //@JsonIgnore 이걸 붙이면 에러 해결할 수 있는데 일단 DTO로 해결..
+    @ManyToOne @JoinColumn(name="user_id")
     private UserEntity user;
 
-    @NonNull
+    @Column(nullable = false)
     private LocalDateTime created_at;
     private LocalDateTime modified_at;
-    @NonNull
+    @Column(nullable = false)
     private LocalDateTime started_at;
-    @NonNull
+    @Column(nullable = false)
     private LocalDateTime ended_at;
-    @NonNull
+    @Column(nullable = false)
     private String comment; // 필수여부?
-    @NonNull
+    @Column(nullable = false)
     private String title;
-    @NonNull
+    @Column(nullable = false)
     private String memo; // 필수여부?
     private String image; // null이면 기본 이미지 보여지게끔
-    @NonNull
+    @Column(nullable = false)
     private String category;
-    @NonNull
+    @Column(nullable = false)
     private Long views;
-    @NonNull
+    @Column(nullable = false)
     private String genre;
-    @NonNull
+    @Column(nullable = false)
     private int platform;
-    @NonNull
+    @Column(nullable = false)
     private int emoji;
+
+    public PostSearchDto toPostSearchDto(PostEntity post){
+        PostSearchDto postSearchDto = new PostSearchDto();
+        postSearchDto.setPost_id(post.getPost_id());
+        postSearchDto.setCreated_at(post.getCreated_at());
+        postSearchDto.setModified_at(post.getModified_at());
+        postSearchDto.setStarted_at(post.getStarted_at());
+        postSearchDto.setEnded_at(post.getEnded_at());
+        postSearchDto.setComment(post.getComment());
+        postSearchDto.setTitle(post.getTitle());
+        postSearchDto.setMemo(post.getMemo());
+        postSearchDto.setImage(post.getImage());
+        postSearchDto.setCategory(post.getCategory());
+        postSearchDto.setViews(post.getViews());
+        postSearchDto.setGenre(post.getGenre());
+        postSearchDto.setPlatform(post.getPlatform());
+        postSearchDto.setEmoji(post.getEmoji());
+        return postSearchDto;
+    }
 }
