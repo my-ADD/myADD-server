bacf2c6886e42d2fa760644ad0160793bbcd8407
bbangjun <lucid9dawn@gmail.com>
2023-07-24 22:08:47 +0900

    feat(SessionManager): 세션 생성, 조회, 만료 1차 구현 완료

---
+++ src/test/java/com/myadd/myadd/user/session/SessionManagerTest.java
@@ -0,0 +1,16 @@
+package com.myadd.myadd.user.session;
+
+import com.myadd.myadd.user.domain.UserEntity;
+import org.apache.catalina.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.http.HttpServletResponse;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionManagerTest {
+
+
+}
