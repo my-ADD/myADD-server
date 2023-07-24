d7e9638613d133f9b805a47b8a9c6ea0260c2a1f
HideOnCodec <tlsdmsgp33@naver.com>
2023-07-24 17:31:04 +0900

    feat(PostSearchService) : 포토카드 앞,뒷 장 조회하는 getFrontPage,getBackPage 서비스 구현

--- src/main/java/com/myadd/myadd/post/search/service/PostSearchService.java
+++ src/main/java/com/myadd/myadd/post/search/service/PostSearchService.java
@@ -2,7 +2,9 @@ package com.myadd.myadd.post.search.service;
 
 import com.myadd.myadd.post.domain.PostEntity;
 import com.myadd.myadd.post.repository.PostRepository;
+import com.myadd.myadd.post.search.dto.PostSearchBackDto;
 import com.myadd.myadd.post.search.dto.PostSearchDto;
+import com.myadd.myadd.post.search.dto.PostSearchFrontDto;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -20,16 +22,43 @@ public class PostSearchService {
 
     //특정 사용자의 전체 게시글을 불러옴
     @Transactional
-    public List<PostSearchDto> getPostList(Long user_id) {
+    public List<PostSearchDto> getPostList(Long userId) {
         List<PostEntity> posts = postRepository.findAll();
         List<PostSearchDto> postSearchDtoList = new ArrayList<>();
 
         for (PostEntity post : posts) {
-            if(post.getUser().getUserId() == user_id) {
+            if(post.getUser().getUserId() == userId) {
                 PostSearchDto postSearchDto = post.toPostSearchDto(post);
                 postSearchDtoList.add(postSearchDto);
             }
         }
         return postSearchDtoList;
+    }
+
+    @Transactional
+    public PostSearchFrontDto getFrontPage(Long postId){
+        PostEntity post = postRepository.findByPostId(postId);
+        PostSearchFrontDto postSearchFrontDto = new PostSearchFrontDto();
+        postSearchFrontDto.setComment(post.getComment());
+        postSearchFrontDto.setImage(post.getImage());
+
+        return postSearchFrontDto;
+    }
+
+    @Transactional
+    public PostSearchBackDto getBackPage(Long postId){
+        PostEntity post = postRepository.findByPostId(postId);
+        PostSearchBackDto postSearchBackDto = new PostSearchBackDto();
+        postSearchBackDto.setMemo(post.getMemo());
+        postSearchBackDto.setEmoji(post.getEmoji());
+        postSearchBackDto.setGenre(post.getGenre());
+        postSearchBackDto.setPlatform(post.getPlatform());
+        postSearchBackDto.setTitle(post.getTitle());
+        postSearchBackDto.setCreatedAt(post.getCreatedAt());
+        postSearchBackDto.setEndedAt(post.getEndedAt());
+        postSearchBackDto.setStartedAt(post.getStartedAt());
+        postSearchBackDto.setViews(post.getViews());
+
+        return postSearchBackDto;
     }
 }
