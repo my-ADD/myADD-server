c7989dd1521658a5519d82fec002118b2394416d
HideOnCodec <tlsdmsgp33@naver.com>
2023-07-24 17:32:59 +0900

    feat(UserTypeEnum) : 테스트를 위한 통일을 위해 feature_signup_email User Domain 복붙

---
+++ src/main/java/com/myadd/myadd/user/domain/UserTypeEnum.java
@@ -0,0 +1,34 @@
+package com.myadd.myadd.user.domain;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonKey;
+import lombok.Getter;
+
+@Getter
+public enum UserTypeEnum {
+    EMAIL("이메일 회원"), // 0
+    NAVER("네이버 회원"), // 1
+    KAKAO("카카오 회원"), // 2
+    GOOGLE("구글 회원"); // 3
+
+    private final String key;
+
+    UserTypeEnum(String key) {
+        this.key = key;
+    }
+
+    @JsonCreator // 역직렬화
+    public static UserTypeEnum from(String value) {
+        for (UserTypeEnum user : UserTypeEnum.values()) {
+            if (user.getKey().equals(value)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    @JsonKey // 직렬화
+    public String getKey() {
+        return key;
+    }
+}
