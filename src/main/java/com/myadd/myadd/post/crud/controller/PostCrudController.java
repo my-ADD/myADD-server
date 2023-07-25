76074325851cdfe9f86839a38cc8b08e0186866e
minsun <pitapatsun@gmail.com>
2023-07-25 17:36:09 +0900

    Add(PostCrudService): 포토카드 수정 구현 완료

--- src/main/java/com/myadd/myadd/post/crud/controller/PostCrudController.java
+++ src/main/java/com/myadd/myadd/post/crud/controller/PostCrudController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 @Slf4j
@@ -15,22 +17,29 @@ public class PostCrudController {
 
     private final PostCrudService postCrudService;
 
+    //포토카드 전체 조회(for test)
     @GetMapping("/home")
-    public String list() {
+    public String list(Model model) {
+        List<PostCrudDto> postList = postCrudService.getPostList();
+
+        model.addAttribute("PostList", postList);
         return "home";
     }
 
+    //포토카드 작성화면으로 이동(for test)
     @GetMapping(value = "/posts/add-post")
     public String create() {
         return "post/write";
     }
 
+    //포토카드 글작성
     @PostMapping(value = "/posts/add-post")
     public String create(PostCrudDto post) {
         postCrudService.savePost(post);
         return "redirect:/home";
     }
 
+    //포토카드 삭제
     @ResponseBody
     @DeleteMapping(value = "/posts/delete-post/{postId}")
     public String delete(@PathVariable("postId") Long id) {
@@ -39,20 +48,29 @@ public class PostCrudController {
        return "delete" + id;
     }
 
-    @ResponseBody
-    @GetMapping("/posts/update-post/{postId}")
-    public String edit(@PathVariable("postId") Long id, Model model) {
-        PostCrudDto postCrudDto = postCrudService.getPost(id);
+    //포토카드 글 조회(for test)
+    @GetMapping("/posts/{postId}")
+    public String detail(@PathVariable("postId") Long id, Model model) {
+        PostCrudDto postCrudDto = postCrudService.findOne(id);
 
         model.addAttribute("postCrudDto", postCrudDto);
-        return "GET mapping " + postCrudDto.toString();
+        return "post/detail";
     }
-//
-//    @ResponseBody
-//    @PutMapping("/posts/update-post/{postId}")
-//    public String update(@RequestBody PostCrudDto postCrudDto) {
-//        log.info(String.valueOf(postCrudDto.getPost_id()));
-//        postCrudService.savePost(postCrudDto);
-//        return "수정완료";
-//    }
+
+    //포토카드 수정 폼 화면(for test)
+    @GetMapping("/posts/update-post/{postId}")
+    public String edit(@PathVariable("postId") Long id, Model model) {
+        PostCrudDto postCrudDto = postCrudService.findOne(id);
+
+        model.addAttribute("postCrudDto", postCrudDto);
+        return "post/update";
+    }
+
+    //포토카드 수정
+    @PutMapping("/posts/update-post/{postId}")
+    public String update(PostCrudDto postCrudDto) {
+        log.info(String.valueOf(postCrudDto.getPostId()));
+        postCrudService.savePost(postCrudDto);
+        return "redirect:/home";
+    }
 }
