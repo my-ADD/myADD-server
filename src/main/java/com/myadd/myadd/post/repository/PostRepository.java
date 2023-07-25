54788cbaf457334239af9307f8c6a57f5ccb351a
HideOnCodec <tlsdmsgp33@naver.com>
2023-07-25 18:22:26 +0900

    feat(PostRepository) : 포토카드 기록순,이름순으로 목록 조회하는 함수 구현

--- src/main/java/com/myadd/myadd/post/repository/PostRepository.java
+++ src/main/java/com/myadd/myadd/post/repository/PostRepository.java
@@ -12,12 +12,12 @@ import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<PostEntity,Long> {
-    /* 유저명에 따른 게시글을 불러오고 싶은데 잘 안된다..
-    외래키와 자동으로 연결되는 걸 보면 그냥 user_id로 유저 찾아서 그 유저의 post를 긁어오는 게 더 간편할 거 같다!
-    @Query(value = "select p from PostEntity p where user_id = ?1")
-    List<PostEntity> findByUser(User user);
-    */
+    // 외래키와 자동으로 연결되는 걸 보면 그냥 user_id로 유저 찾아서 그 유저의 post를 긁어오는 게 더 간편할 거 같다!
 
+    // 포토카드 전체 목록 조회(기록순)
+    List<PostEntity> findAllByOrderByCreatedAtDesc();
+    // 포토카드 전체 목록 조회(이름순)
+    List<PostEntity> findAllByOrderByTitle();
     // 포토카드 하나 조회
     PostEntity findByPostId(Long postId);
 }
