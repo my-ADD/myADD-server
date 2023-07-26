4fc6dfa07063eef6aca83c300860a45fb169e669
bbangjun <lucid9dawn@gmail.com>
2023-07-26 22:46:24 +0900

    feat(KakaoLoginService): access token 발급 완료

--- src/main/java/com/myadd/myadd/user/sigunup/kakao/controller/KakaoLoginController.java
+++ src/main/java/com/myadd/myadd/user/sigunup/kakao/controller/KakaoLoginController.java
@@ -1,4 +1,21 @@
 package com.myadd.myadd.user.sigunup.kakao.controller;
 
+import com.myadd.myadd.user.sigunup.kakao.service.KakaoLoginService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/login/oauth2", produces = "application/json")
 public class KakaoLoginController {
+
+    private final KakaoLoginService kakaoLoginService;
+
+    @GetMapping("/code/kakao/kakao")
+    public @ResponseBody String kakaoCallback(@RequestParam String code){
+
+        String accessToken = kakaoLoginService.getAccessToken(code);
+        return accessToken;
+    }
 }
--- src/main/java/com/myadd/myadd/user/sigunup/kakao/service/KakaoLoginService.java
+++ src/main/java/com/myadd/myadd/user/sigunup/kakao/service/KakaoLoginService.java
@@ -1,4 +1,42 @@
 package com.myadd.myadd.user.sigunup.kakao.service;
 
+import com.myadd.myadd.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
 public class KakaoLoginService {
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final UserRepository userRepository;
+
+    public String getAccessToken(String code){
+
+        HttpHeaders headers = new HttpHeaders();;
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", "f7dee74aded517904bcbea4c811ada0f");
+        params.add("redirect_uri", "http://localhost:8080/login/oauth2/code/kakao/kakao");
+        params.add("code", code);
+
+        HttpEntity<MultiValueMap<String, String>> accessTokenRequest = new HttpEntity<>(params, headers);
+
+        ResponseEntity response = restTemplate.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                accessTokenRequest,
+                String.class
+        );
+
+        return response.toString();
+    }
 }
