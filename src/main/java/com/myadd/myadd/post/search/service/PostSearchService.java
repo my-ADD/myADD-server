998d4dc4704cd159e68ec09ce6d624099a3ff761
HideOnCodec <tlsdmsgp33@naver.com>
2023-07-26 21:01:21 +0900

    feat(PostSearchService) : getPostList 수정 & getPostListByPlatform 구현(페이징 적용)

--- src/main/java/com/myadd/myadd/post/search/service/PostSearchService.java
+++ src/main/java/com/myadd/myadd/post/search/service/PostSearchService.java
@@ -5,6 +5,10 @@ import com.myadd.myadd.post.repository.PostRepository;
 import com.myadd.myadd.post.search.dto.PostSearchBackDto;
 import com.myadd.myadd.post.search.dto.PostSearchDto;
 import com.myadd.myadd.post.search.dto.PostSearchFrontDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -23,16 +27,16 @@ public class PostSearchService {
     //특정 사용자의 포토카드 전체 목록 조회(기록순,이름순)
     //flag 0: 기록순, 1:이름순
     @Transactional
-    public List<PostSearchDto> getPostList(Long userId, int flag) {
-        List<PostEntity> posts;
-        if(flag == 0)
-            posts = postRepository.findAllByOrderByCreatedAtDesc();
-        else
-            posts = postRepository.findAllByOrderByTitle();
+    public List<PostSearchDto> getPostList(Long userId, int flag, int page) {
+        Page<PostEntity> posts;
+        if (flag == 0) // 기록순
+            posts = postRepository.findAll(PageRequest.of(page, 2, Sort.by(Sort.Direction.DESC, "createdAt")));
+        else // 이름순
+            posts = postRepository.findAll(PageRequest.of(page, 2, Sort.by(Sort.Direction.DESC, "title")));
         List<PostSearchDto> postSearchDtoList = new ArrayList<>();
 
         for (PostEntity post : posts) {
-            if(post.getUser().getUserId() == userId) {
+            if (post.getUser().getUserId() == userId) {
                 PostSearchDto postSearchDto = post.toPostSearchDto(post);
                 postSearchDtoList.add(postSearchDto);
             }
@@ -40,8 +44,32 @@ public class PostSearchService {
         return postSearchDtoList;
     }
 
+    //특정 사용자의 포토카드 플랫폼 목록 조회(기록순,이름순)
+    //flag 0: 기록순, 1:이름순
     @Transactional
-    public PostSearchFrontDto getFrontPage(Long postId){
+    public List<PostSearchDto> getPostListByPlatform(Long userId, int flag, String category,int platform, int page) {
+        Page<PostEntity> posts;
+        Pageable pagingByCreatedAt = PageRequest.of(page, 2, Sort.by(Sort.Direction.DESC, "createdAt"));
+        Pageable pagingByTitle = PageRequest.of(page, 2, Sort.by(Sort.Direction.DESC, "title"));
+
+        if (flag == 0)
+            posts = postRepository.findByPlatformAndCategoryOrderByCreatedAtDesc(platform,category,pagingByCreatedAt);
+        else
+            posts = postRepository.findByPlatformAndCategoryOrderByTitle(platform,category,pagingByTitle);
+        List<PostSearchDto> postSearchDtoList = new ArrayList<>();
+
+        for (PostEntity post : posts) {
+            if (post.getUser().getUserId() == userId) {
+                PostSearchDto postSearchDto = post.toPostSearchDto(post);
+                postSearchDtoList.add(postSearchDto);
+            }
+        }
+        return postSearchDtoList;
+    }
+
+    // 포토 카드 하나의 상세정보 (앞페이지)
+    @Transactional
+    public PostSearchFrontDto getFrontPage(Long postId) {
         PostEntity post = postRepository.findByPostId(postId);
         PostSearchFrontDto postSearchFrontDto = new PostSearchFrontDto();
         postSearchFrontDto.setComment(post.getComment());
@@ -49,9 +77,9 @@ public class PostSearchService {
 
         return postSearchFrontDto;
     }
-
+    // 포토 카드 하나의 상세정보 (뒷페이지)
     @Transactional
-    public PostSearchBackDto getBackPage(Long postId){
+    public PostSearchBackDto getBackPage(Long postId) {
         PostEntity post = postRepository.findByPostId(postId);
         PostSearchBackDto postSearchBackDto = new PostSearchBackDto();
         postSearchBackDto.setMemo(post.getMemo());
