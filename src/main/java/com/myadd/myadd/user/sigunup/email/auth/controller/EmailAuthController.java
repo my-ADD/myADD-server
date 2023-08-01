9f9115d74857190d343b89db854d186847d19bac
bbangjun <lucid9dawn@gmail.com>
2023-08-01 12:44:01 +0900

    refactor(feature_signup_email): base url 변경

--- src/main/java/com/myadd/myadd/user/sigunup/email/auth/controller/EmailAuthController.java
+++ src/main/java/com/myadd/myadd/user/sigunup/email/auth/controller/EmailAuthController.java
@@ -14,13 +14,14 @@ import java.util.concurrent.TimeUnit;
 
 // 비밀번호 찾기를 위해 사용하는 이메일 인증
 @RestController
-@RequestMapping(value = "/users/join/email")
+@RequestMapping(value = "/users/change-password")
 @RequiredArgsConstructor
 public class EmailAuthController {
     private final EmailAuthService emailService;
     private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
 
-    @PostMapping("/send-code") // /users/join/email/send-code
+    // 이메일 인증 코드 전송
+    @PostMapping("/email/send-code") // /users/join/email/send-code
     public String sendEmail(@RequestBody EmailAuthRequestDto emailAuthRequestDto) throws MessagingException, UnsupportedEncodingException {
         String authCode = emailService.sendEmail(emailAuthRequestDto.getEmail());
 
@@ -29,10 +30,15 @@ public class EmailAuthController {
         return authCode;
     }
 
-    @PostMapping("/check-code")
+    // 사용자가 입력한 인증 코드와 db의 인증 정보 비교
+    @PostMapping("/email/check-code")
     public String checkCode(@RequestParam String email, @RequestParam String code){
 
         return emailService.verifyCode(email, code);
+    }
+
+    public String changePassword(@RequestParam ){
+
     }
 
     @PreDestroy
