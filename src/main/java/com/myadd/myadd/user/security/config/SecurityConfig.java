4acdf190f8f74d57b4d194c4a42641f03a138e4a
bbangjun <lucid9dawn@gmail.com>
2023-08-02 23:16:16 +0900

    feat(SecurityConfig): 로그아웃 url 설정 및 성공 시의 response 생성

--- src/main/java/com/myadd/myadd/user/security/config/SecurityConfig.java
+++ src/main/java/com/myadd/myadd/user/security/config/SecurityConfig.java
@@ -5,8 +5,15 @@ import com.myadd.myadd.user.security.handler.CustomAuthenticationSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 public class SecurityConfig {
@@ -27,10 +34,19 @@ public class SecurityConfig {
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
-                .loginProcessingUrl("/login") // 시큐리티가 /login 경로로 로직을 만들어서 시큐리티 로그인을 처리함
+                .loginProcessingUrl("/users/login/email") // 시큐리티가 /login 경로로 로직을 만들어서 시큐리티 로그인을 처리함
                 .successHandler(new CustomAuthenticationSuccessHandler())
                 .failureHandler(new CustomAuthenticationFailureHandler())
                 .usernameParameter("email");
+        httpSecurity.logout()
+                .logoutUrl("/users/my-info/logout")
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.setStatus(HttpServletResponse.SC_OK);
+                        response.getWriter().write("{\"message\": \"Logout success!\"}");
+                    }
+                });
 
 
         return httpSecurity.build();
--- src/main/java/com/myadd/myadd/user/sigunup/email/controller/EmailLoginController.java
+++ src/main/java/com/myadd/myadd/user/sigunup/email/controller/EmailLoginController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.util.Enumeration;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -51,10 +52,9 @@ public class EmailLoginController {
     }
 
     @PostMapping("/login/email") // 로그인(아이디, 비밀번호)  (로그인을 하기 위해 입력하는 창에서의 로직(실제 로그인))
-    public String login(@ModelAttribute EmailLoginForm emailLoginForm, HttpServletRequest request){
+    public @ResponseBody String login(@RequestParam String email, @RequestParam String password,  HttpServletRequest request){
 
-        UserEntity loginUser = emailLoginService.emailLogin(emailLoginForm.getLoginEmail(), emailLoginForm.getLoginPassWord());
-        log.info("login? {}", loginUser);
+        UserEntity loginUser = emailLoginService.emailLogin(email, password);
 
         // 로그인 성공 처리
         // 세션이 있으면 있는 세션 반환, 없으면 신규 세션을 생성
@@ -62,21 +62,17 @@ public class EmailLoginController {
         // 세션에 로그인 회원 정보 보관
         session.setAttribute(AppConstants.LOGIN_MEMBER, loginUser);
 
-        return "login success! go to individual page";
-    }
-
-    @GetMapping("/") // 로그인되어 있는지 확인하고, 그에 따라 적절한 페이지로 이동
-    public String homeLogin(
-            @SessionAttribute(name = AppConstants.LOGIN_MEMBER, required = false) UserEntity userEntity, Model model){
-
-        // 세션에 회원 데이터가 없으면 home
-        if (userEntity == null){
-            return "home";
+        if (session != null) {
+            Enumeration<String> attributeNames = session.getAttributeNames();
+            while (attributeNames.hasMoreElements()) {
+                String attributeName = attributeNames.nextElement();
+                Object attributeValue = session.getAttribute(attributeName);
+                log.info("Session attribute: {} = {}", attributeName, attributeValue);
+            }
+        } else {
+            log.info("No active session found");
         }
-
-        // 세션이 유지되면 개인 화면 유지
-        model.addAttribute("user", userEntity);
-        return "login status continue!! go to individual page";
+        return "login success! go to individual page";
     }
 
     @PostMapping("/my-info/logout")
