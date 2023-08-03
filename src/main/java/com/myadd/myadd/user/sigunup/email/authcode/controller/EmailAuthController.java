51423d5d811e60bb78b592f8768968a6534939ea
bbangjun <lucid9dawn@gmail.com>
2023-08-04 03:36:11 +0900

    feat(EmailAuthController): 비밀번호 찾기 및 비밀번호 변경 과정에서 항상 이메일 회원이 맞는지 확인   로직 추가

--- src/main/java/com/myadd/myadd/user/sigunup/email/authcode/controller/EmailAuthController.java
+++ src/main/java/com/myadd/myadd/user/sigunup/email/authcode/controller/EmailAuthController.java
@@ -13,7 +13,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-// 비밀번호 찾기를 위해 사용하는 이메일 인증
+// 비밀번호 찾기 및 비밀번호 변경
 @Slf4j
 @RestController
 @RequestMapping(value = "/change-password")
@@ -27,12 +27,12 @@ public class EmailAuthController {
         String authCode="";
 
         log.info("first = {}", emailAuthRequestDto.getEmail());
-        // 이메일로 회원가입한 유저가 아닌 경우 예외
-        if(emailService.isUserTypeEmail(emailAuthRequestDto.getEmail())){
-            emailService.deleteExistCode(emailAuthRequestDto.getEmail());
-            authCode = emailService.sendEmail(emailAuthRequestDto.getEmail());
+            // 이메일로 회원가입한 유저가 아닌 경우 예외
+            if(emailService.isUserTypeEmail(emailAuthRequestDto.getEmail())){
+                emailService.deleteExistCode(emailAuthRequestDto.getEmail());
+                authCode = emailService.sendEmail(emailAuthRequestDto.getEmail());
 
-            executorService.schedule(emailService::deleteExpiredAuthNum, 5, TimeUnit.MINUTES);
+                executorService.schedule(emailService::deleteExpiredAuthNum, 5, TimeUnit.MINUTES);
 
             return authCode;
         }
@@ -44,13 +44,23 @@ public class EmailAuthController {
     // 사용자가 입력한 인증 코드와 db의 인증 정보 비교
     @PostMapping("/check-code") // 이메일 인증번호 확인
     public String verifyCode(@RequestParam String email, @RequestParam String code){
-
-        return emailService.verifyCode(email, code);
+        if(emailService.isUserTypeEmail(email)) {
+            return emailService.verifyCode(email, code);
+        }
+        else{
+            return "not email user";
+        }
     }
 
     @PutMapping("") // 비밀번호 변경
     public String changePassword(@RequestParam String email, @RequestParam String password){
-        return emailService.changePassword(email, password);
+        if(emailService.isUserTypeEmail(email)) {
+            return emailService.changePassword(email, password);
+        }
+        else{
+            return "not email user";
+        }
+
     }
 
     @PreDestroy
