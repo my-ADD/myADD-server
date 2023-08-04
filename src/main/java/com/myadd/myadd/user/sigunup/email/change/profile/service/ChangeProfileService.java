43d91d204f19adca7fe792f34c01ba12582368ed
bbangjun <lucid9dawn@gmail.com>
2023-08-04 22:54:26 +0900

    feat(ChangeProfileService): 유저 프로필 변경을 위해 유저 찾기 메서드 및 프로필 변경 메서드 구현

--- src/main/java/com/myadd/myadd/user/sigunup/email/change/profile/service/ChangeProfileService.java
+++ src/main/java/com/myadd/myadd/user/sigunup/email/change/profile/service/ChangeProfileService.java
@@ -15,5 +15,23 @@ public class ChangeProfileService {
     @Autowired
     private final UserRepository userRepository;
 
+    public UserEntity findUser(Long id, String email) {
+        if(userRepository.findById(id).isPresent())
+            return userRepository.findByEmail(email).get();
+        else
+            return null;
+    }
 
+    public String changeProfile(UserEntity user, String nickname, String profile) {
+
+        if(user != null){
+            user.setNickname(nickname);
+            user.setProfile(profile);
+            userRepository.save(user);
+            return "Profile change success!";
+        }
+        else{
+            return "Profile change fail!";
+        }
+    }
 }
