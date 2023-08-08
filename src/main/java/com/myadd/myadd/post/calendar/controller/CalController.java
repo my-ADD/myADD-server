aff98f46bc395dc773a80fc175d31baea4c0b34b
minsun <pitapatsun@gmail.com>
2023-08-09 00:23:57 +0900

    refactor(CalController, PostCrudController):파라미터 받는 방식 통일

--- src/main/java/com/myadd/myadd/post/calendar/controller/CalController.java
+++ src/main/java/com/myadd/myadd/post/calendar/controller/CalController.java
@@ -2,8 +2,11 @@ package com.myadd.myadd.post.calendar.controller;
 
 import com.myadd.myadd.post.calendar.service.CalService;
 import com.myadd.myadd.post.crud.dto.PostBackDto;
+import com.myadd.myadd.user.security.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -17,10 +20,11 @@ public class CalController {
     private final CalService calService;
 
     ///posts/calendar/2023-07-29?userId=1
-    @GetMapping("/posts/calendar/{createdAt}")
+    @GetMapping("/posts/calendar")
     @ResponseBody
-    public List<PostBackDto> postListCreatedAt(@RequestParam(required = false) Long userId,
-                                               @PathVariable String createdAt, Model model) {
+    public List<PostBackDto> postListCreatedAt(@RequestParam(name="createdAt") String createdAt, Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Long userId = ((PrincipalDetails) authentication.getPrincipal()).getId();
         List<PostBackDto> postBackDtoList = calService.findByCreatedAt(userId, createdAt);
         model.addAttribute("postList", postBackDtoList);
 
