64a78ab1fb6f1d95cf132b90a7b7d6b44ba30908
bbangjun <lucid9dawn@gmail.com>
2023-08-11 11:20:07 +0900

    feat(PrincipalOauth2UserService): Oauth2 로그인 이후 white label error 페이지 대신 json 타입의 성공 메시지 반환 로직 추가

--- src/main/java/com/myadd/myadd/response/BaseResponseStatus.java
+++ src/main/java/com/myadd/myadd/response/BaseResponseStatus.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum BaseResponseStatus {
 
+    // 1000 : 요청 성공
     SUCCESS(true, 1000, "요청에 성공하였습니다."),
     SUCCESS_EMAIL_SIGNUP(true, 1001, "이메일 회원가입을 성공하였습니다."),
     SUCCESS_EMAIL_LOGIN(true, 1002, "이메일 로그인에 성공하였습니다."),
@@ -23,7 +24,7 @@ public enum BaseResponseStatus {
         this.message = message;
     }
 
-    // 1000 : 요청 성공
+
 
     // 2000 : Request 오류(프론트엔드 측 오류) ex) 값을 넘기지 않았을 때
     // 3000 : Response 오류(request는 정상적으로 수행됨)
