175a9ba71c8b1112821ddfe44cdcfb697d150ff0
bbangjun <lucid9dawn@gmail.com>
2023-08-14 01:03:31 +0900

    feat(CustomAuthenticationSuccessHandler): 이메일 로그인 성공 json response 추가

--- src/main/java/com/myadd/myadd/user/security/handler/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/myadd/myadd/user/security/handler/CustomAuthenticationSuccessHandler.java
@@ -1,5 +1,8 @@
 package com.myadd.myadd.user.security.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.myadd.myadd.response.BaseResponse;
+import com.myadd.myadd.response.BaseResponseStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -13,13 +16,23 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
         AuthenticationSuccessHandler.super.onAuthenticationSuccess(request, response, chain, authentication);
+        BaseResponse<BaseResponseStatus> successResponse = new BaseResponse<>(BaseResponseStatus.SUCCESS_EMAIL_LOGIN);
+        String jsonResponse = new ObjectMapper().writeValueAsString(successResponse);
+
         response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().write("{\"message\": \"Login successful\"}");
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(jsonResponse);
     }
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        BaseResponse<BaseResponseStatus> successResponse = new BaseResponse<>(BaseResponseStatus.SUCCESS_EMAIL_LOGIN);
+        String jsonResponse = new ObjectMapper().writeValueAsString(successResponse);
+
         response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().write("{\"message\": \"Login successful\"}");
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(jsonResponse);
     }
 }
