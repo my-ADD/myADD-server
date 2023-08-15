71fc420f14a05ed6f30518fdb70827cf3e858d78
minsun <pitapatsun@gmail.com>
2023-08-15 19:41:31 +0900

    refactor(PostBackDto): 포토카드 null 관련 코드 삭제

--- src/main/java/com/myadd/myadd/post/crud/dto/PostBackDto.java
+++ src/main/java/com/myadd/myadd/post/crud/dto/PostBackDto.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor(force = true)
@@ -20,34 +19,24 @@ public class PostBackDto {
 
     private LocalDateTime modifiedAt;
 
-    @NotNull(message = "시청한 시작일을 입력해주세요")
     private String startedAt;
 
-    @NotNull (message = "시청한 마지막날을 입력해주세요")
     private String endedAt;
 
-    @NotNull(message = "제목을 입력해주세요")
     private String title;
 
-    @NotNull(message = "메모를 입력해주세요")
     private String memo;
 
-    @NotNull(message = "카테고리를 입력해주세요")
     private String category;
 
-    @NotNull(message = "시청횟수를 입력해주세요")
     private Long views;
 
-    @NotNull(message = "장르를 입력해주세요")
     private String genre;
 
-    @NotNull(message = "플랫폼을 선택해주세요")
     private String platform;
 
-    @NotNull(message = "이모지를 입력해주세요")
     private String emoji;
 
-    @NotNull(message = "코멘트를 입력해주세요")
     private String comment;
 
     private String image; // null이면 기본 이미지 보여지게끔
