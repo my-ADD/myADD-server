25306e0061df12e01a38bab236ec95c00717073f
HideOnCodec <tlsdmsgp33@naver.com>
2023-09-05 17:04:03 +0900

    feat(PostRepository) : paging없이 List로 DB값 반환하도록 수정

--- src/main/java/com/myadd/myadd/post/repository/PostRepository.java
+++ src/main/java/com/myadd/myadd/post/repository/PostRepository.java
@@ -2,8 +2,6 @@ package com.myadd.myadd.post.repository;
 
 import com.myadd.myadd.post.domain.PostEntity;
 import com.myadd.myadd.user.domain.entity.UserEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,14 +11,14 @@ import java.util.List;
 @Repository
 public interface PostRepository extends JpaRepository<PostEntity,Long> {
     // 전체조회 시 And 조건을 이용하여 User까지 긁어오면 더 코드가 간편해질거같다!
-    // 사용자의 포토카드 전체 리스트 조회(pageable 제외)
-    List<PostEntity> findByUser(UserEntity user);
-    // 사용자의 포토카드 전체 리스트 조회
-    Page<PostEntity> findByUser(UserEntity user, Pageable pageable);
+    // 사용자의 포토카드 전체 리스트 조회(기록순)
+    List<PostEntity> findByUserOrderByCreatedAtDesc(UserEntity user);
+    // 사용자의 포토카드 전체 리스트 조회(이름순)
+    List<PostEntity> findByUserOrderByTitle(UserEntity user);
     // 포토카드 플랫폼에 따른 목록 조회(기록순)
-    Page<PostEntity> findByPlatformAndCategoryAndUserOrderByCreatedAtDesc(String platform, String category, UserEntity user, Pageable pageable);
+    List<PostEntity> findByPlatformAndCategoryAndUserOrderByCreatedAtDesc(String platform, String category, UserEntity user);
     // 포토카드 플랫폼에 따른 목록 조회(이름순)
-    Page<PostEntity> findByPlatformAndCategoryAndUserOrderByTitle(String platform,String category,UserEntity user,Pageable pageable);
+    List<PostEntity> findByPlatformAndCategoryAndUserOrderByTitle(String platform,String category,UserEntity user);
     // 포토카드 하나 조회
     PostEntity findByPostId(Long postId);
 }
